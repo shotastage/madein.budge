c348549309cf598c79585d01f4caf7d13901720f
Shota Shimazu <hornet.live.mf@gmail.com>
2022-06-23 01:57:08 +0900

    add budge server

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 func main() {
-	println("THIS IS A PEN.")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/", serveBudge)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func serveBudge(c echo.Context) error {
+	f, err := os.Open("assets/budge.png")
+	if err != nil {
+		return err
+	}
+	return c.Stream(http.StatusOK, "image/png", f)
+
+	// return c.String(http.StatusOK, "Hello, World!")
 }
